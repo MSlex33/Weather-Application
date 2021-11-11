09a5bfad47e57f8a66df3f2e7f8199e6bf88557e
Alex <mslex.exe@gmail.com>
2021-11-11 09:09:27 +0300

    Initial Commit

---
+++ Weather Application/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather Application
+//
+//  Created by Alex Popov on 11.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
